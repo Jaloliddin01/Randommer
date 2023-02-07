b761322d60ab542d74629baf4d1096140d1e7a8a
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-02-07 16:58:04 +0500

    finished name.py

--- name.py
+++ name.py
@@ -14,7 +14,11 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        payload = {'nameType': nameType, 'quantity': quantity}
+        url = f'{self.base_url}Name'
+        response = requests.get(url, headers=headers, params=payload)
+        return response.json()
     
     def get_name_suggestions(self, api_key: str, startingWords: str) -> list:
         '''get name suggestions
@@ -26,7 +30,11 @@ class Name(Randommer):
         Returns:
             list: list of name suggestions
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        payload = {'startingWords': startingWords}
+        url = f'{self.base_url}Name/Suggestions'
+        response = requests.get(url, headers=headers, params=payload)
+        return response.json()
     
     def get_name_cultures(self, api_key: str) -> list:
         '''get available cultures
@@ -37,4 +45,13 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        url = f'{self.base_url}Name/Cultures'
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+apikey = '1342a09ae7384e94a5aa8f52febf9fa3'
+obj = Name()
+# print(obj.get_name(api_key=apikey, nameType='firstname', quantity=1))
+# print(obj.get_name_suggestions(api_key=apikey, startingWords='john'))
+# print(obj.get_name_cultures(apikey))
