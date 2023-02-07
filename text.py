d30cf865a4320474dfc7140fb76341000f682eda
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-02-07 17:11:06 +0500

    finished text.py

--- text.py
+++ text.py
@@ -15,7 +15,11 @@ class Text(Randommer):
         Returns:
             str: Lorem text
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        payload = {'loremType': loremType, 'type': type, 'number': number}
+        url = f'{self.base_url}Text/LoremIpsum'
+        response = requests.get(url, headers=headers, params=payload)
+        return response.json()
     
     def generate_password(self, api_key: str, length: int, hasDigits: bool, hasUppercase: bool, hasSpecial: bool) -> str:
         '''Generate lorem ipsum
@@ -30,4 +34,13 @@ class Text(Randommer):
         Returns:
             str: pasword
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        payload = {'length': length, 'hasDigits': hasDigits, 'hasUppercase': hasUppercase, 'hasSpecial': hasSpecial}
+        url = f'{self.base_url}Text/Password'
+        response = requests.get(url, headers=headers, params=payload)
+        return response.json()
+
+apikey = '1342a09ae7384e94a5aa8f52febf9fa3'
+obj = Text()
+print(obj.generate_LoremIpsum(apikey, 'normal', 'words', 5))
+print(obj.generate_LoremIpsum(apikey, 'bussines', 'paragraphs', 5))
