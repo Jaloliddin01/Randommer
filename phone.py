f645532c89fbc2fa34e4f47f34dc613e04ab56d0
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-02-07 17:04:28 +0500

    finished phone.py

--- phone.py
+++ phone.py
@@ -14,7 +14,11 @@ class Phone(Randommer):
         Returns:
             list: list of phone numbers
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        payload = {'CountryCode': CountryCode, 'Quantity': Quantity}
+        url = f'{self.base_url}Phone/Generate'
+        response = requests.get(url, headers=headers, params=payload)
+        return response.json()
     
     def get_IMEI(self, api_key: str, Quantity: int) -> list:
         '''get bulk imei
@@ -26,7 +30,11 @@ class Phone(Randommer):
         Returns:
             list: list of phone numbers
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        payload = {'Quantity': Quantity}
+        url = f'{self.base_url}Phone/IMEI'
+        response = requests.get(url, headers=headers, params=payload)
+        return response.json()
     
     def is_valid(self, api_key: str, telephone: str, CountryCode: str) -> bool:
         '''get bulk imei
@@ -39,7 +47,11 @@ class Phone(Randommer):
         Returns:
             bool: is valid
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        payload = {'telephone': telephone, 'CountryCode': CountryCode}
+        url = f'{self.base_url}Phone/Validate'
+        response = requests.get(url, headers=headers, params=payload)
+        return response.json()
     
     def get_countries(self, api_key: str) -> list:
         '''get countries
@@ -50,4 +62,15 @@ class Phone(Randommer):
         Returns:
             list: lsit of countries
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        url = f'{self.base_url}Phone/Countries'
+        response = requests.get(url, headers=headers)
+
+        return response.json()
+
+apikey = '1342a09ae7384e94a5aa8f52febf9fa3'
+obj = Phone()
+print(obj.generate(apikey, 'uz', 5))
+print(obj.get_IMEI(apikey, 5))
+print(obj.is_valid(apikey, '+998909999999', 'uz'))
+print(obj.get_countries(apikey))
