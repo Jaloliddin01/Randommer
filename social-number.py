24907f72194b1bed5e2eb239ca083e46a8e58027
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-02-07 17:07:43 +0500

    finished social-number.py

--- social-number.py
+++ social-number.py
@@ -12,4 +12,11 @@ class SocialNumber(Randommer):
         Returns:
             str: number as str
         '''
-        pass
+        headers = {'X-Api-Key': api_key}
+        url = f'{self.base_url}SocialNumber'
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+apikey = '1342a09ae7384e94a5aa8f52febf9fa3'
+obj = SocialNumber()
+print(obj.get_SocialNumber(apikey))
